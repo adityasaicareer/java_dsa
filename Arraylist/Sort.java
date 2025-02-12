810c0469b20eb79278866469612c589a3cffff89
Adityasai <adityasaicareer@gmail.com>
2025-02-12 09:01:51 +0530

    sorted arraylist

---
+++ Arraylist/Sort.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Sort {
+
+    public static void main(String args[])
+    {
+        // Collectins.sort();
+        ArrayList<Integer> list=new ArrayList<Integer>();
+        list.add(1);
+        list.add(5);
+        list.add(2);
+        list.add(9);
+        list.add(4);
+
+        Collections.sort(list);
+
+        System.out.println(list);
+        // it will be sorted in ascending order
+        
+
+    }
+    
+}
